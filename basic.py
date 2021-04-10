77b3a09370bce83609ea1ecbc66fde5332b84502
Alt70155 <chika1086@gmail.com>
2021-04-10 22:19:27 +0900

    メソッドベースディスパッチャ

--- basic.py
+++ basic.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template, request
+from flask.views import MethodView
 
 # Flaskインスタンスの作成
 app = Flask(__name__)
 
 # デコレータ
-@app.route('/hello')
-def hello_world():
-    return 'Welcome to Flask world!'
+# @app.route('/hello')
+# def hello_world():
+#     return 'Welcome to Flask world!'
 
 @app.route('/', methods=['GET'])
 def index():
-    title = 'Form Sample'
+    title = 'Sample App'
+    message = 'This is Index Page'
     # templatesディレクトリの中からindex.htmlを検索
-    return render_template('index.html', title=title)
+    return render_template('index.html', title=title, message=message)
 
 @app.route('/<id>/<password>')
 def index2(id, password):
@@ -21,11 +23,32 @@ def index2(id, password):
     return render_template('index.html', title=msg)
 
 #  formの値受け取り
-@app.route('/', methods=['POST'])
-def form():
-    field = request.form['field']
-    return render_template('index.html', title="Form Sample Display", title2=f'{field}')
+# @app.route('/', methods=['POST'])
+# def form():
+#     field = request.form['field']
+#     return render_template('index.html', title="Form Sample Display", title2=f'{field}')
 
+@app.route('/next', methods=['GET'])
+def next():
+    data = ['One', 'Two', 'Three']
+    return render_template('next.html', title='Next page', message='This is Next Page', data=data)
+
+class HelloAPI(MethodView):
+    send = ''
+
+    def get(self):
+        title = 'Next Page GET'
+        msg = '何か入力してください'
+        return render_template('next.html', title=title, message=msg, send=HelloAPI.send)
+
+    def post(self):
+        HelloAPI.send = request.form.get('send')
+        title = 'Next Page POST'
+        msg = f'You send: {HelloAPI.send}'
+        return render_template('next.html', title=title, message=msg, send=HelloAPI.send)
+
+
+app.add_url_rule('/hello/', view_func=HelloAPI.as_view(('hello')))
 
 if __name__ == '__main__':
     app.debug = True
